7b6e0e80d52074d792a97096df04f20d2690849e
crispytwig <48872606+crispytwig@users.noreply.github.com>
2022-03-16 15:47:59 -0500

    [~] Small patch

--- src/main/java/crispytwig/heftycrops/block/CropBlock.java
+++ src/main/java/crispytwig/heftycrops/block/CropBlock.java
@@ -5,7 +5,7 @@ import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.Material;
 
-public class CropBlock extends BushBlock {
+public class CropBlock extends Block {
     public CropBlock() {
         super(Block.Properties.of(Material.VEGETABLE).strength(0.5F).sound(SoundType.SHROOMLIGHT));
     }
